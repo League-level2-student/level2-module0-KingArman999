5e9a5bf1731b258d4419c9594e57384dcfb89d50
League Student <league@iMac-24.attlocal.net>
2020-01-08 19:56:11 -0800

    ArraysSheet,RobotRace, Almost Hiddenbitton

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,55 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
+		Random ran = new Random();
+		
 		//1. make an array of 5 Strings
-		
+		String[] names = new String[5];
 		//2. print the third element in the array
-		
+		System.out.println(names[2]);
 		//3. set the third element to a different value
-		
+		names [2]= "John";
 		//4. print the third element again
-		
+		System.out.println(names[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
 		//6. make an array of 50 integers
-		
+		int [] integers = new int [50];
 		//7. use a for loop to make every value of the integer array a random number
 		
+		for (int i = 0; i < integers.length; i++) {
+			integers[i]= ran.nextInt(50);
+			System.out.println(integers[i]);
+		}
+		int Smallest = integers[0];
+		for (int i = 0; i < integers.length; i++) {
+		if (integers[i]<Smallest) {
+				Smallest = integers[i];
+		}
+		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		System.out.println(Smallest);
 		//9 print the entire array to see if step 8 was correct
 		
 		//10. print the largest number in the array.
+		for (int i = 0; i < integers.length; i++) {
+			integers[i]= ran.nextInt(50);
+			System.out.println(integers[i]);
+		}
+		int largest = integers[0];
+		for (int i = 0; i < integers.length; i++) {
+			if (integers[i]>largest) {
+				largest = integers[i];
+		}
+		}
+		System.out.println(largest);
+		}
 	}
-}
--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -20,10 +20,10 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] buttons;
 	//2 create an int variable called hiddenButton
-	
-	public static void main(String[] args) {
+	int hiddenButton;
+	 static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
 	
@@ -33,29 +33,35 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
-		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		String num = JOptionPane.showInputDialog("Enter a positive number.");
+		int positiveNum = Integer.parseInt(num);
+ 		//4. Initialize the array of JButtons to be the size of the int created in step 3
+		buttons = new JButton[positiveNum];
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
-		
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i]= new JButton();
+			buttons[i].addActionListener(this);
+			panel.add(buttons[i]);
+		}
 		//9 add the panel to the window
-		
+		window.add(panel);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null, "Find the right button out of all the buttons.");
 		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
-		
+		Random ran = new Random(<positiveNum);
+		hiddenButton = ran; 
 		//14. Set the text of the JButton located at hiddenButton to  "ME"
-
+		
 		//15. Use Thread.sleep(1000); to pause the program.
 		//    Surround it with a try/catch - use Eclipse helper for this
-		
+		Thread.sleep(1000);
 		//16. Set the text of the JButton located at hiddenButton to be blank.
 		
 	}
@@ -65,7 +71,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		JButton buttonClicked = (JButton)e.getSource();
 		
 		//17. if the hiddenButton is clicked, tell the user that they win.
-		
+		if (hidden) {
+			
+		}
 		//18. else tell them to try again
 	}
 }
